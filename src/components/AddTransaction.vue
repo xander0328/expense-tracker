fad5dbc378451658c087bed58ca322970b66cf9c
Li Xander <lixandersalvador0@gmail.com>
2024-01-26 11:34:23 +0800

    Signed-off-by: Li Xander <lixandersalvador0@gmail.com>

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -2,18 +2,17 @@
   <h3>Add new transaction</h3>
   <form id="form" @submit.prevent="onSubmit">
     <div class="form-control">
-      <label for="text">Text</label>
-      <input type="text" id="text" placeholder="Enter text..." v-model="text" />
+      <label for="text">Description</label>
+      <input type="text" id="text" placeholder="Enter description" v-model="text" />
     </div>
     <div class="form-control">
       <label for="amount"
-        >Amount <br />
-        (negative - expense, positive - income)</label
+        >Amount</label
       >
       <input
-        type="text"
+        type="number"
         id="amount"
-        placeholder="Enter amount..."
+        placeholder="Enter amount"
         v-model="amount"
       />
     </div>
